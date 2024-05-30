1f99ac4444edfcbc8dd0b84f1722b40eca34de8b
Bomin Kwon <101239440+pingowl@users.noreply.github.com>
2024-05-30 14:37:23 +0900

    Feat: monitoring setup (#28)

--- src/main/java/com/drugbox/DrugBoxApplication.java
+++ src/main/java/com/drugbox/DrugBoxApplication.java
@@ -1,7 +1,9 @@
 package com.drugbox;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,4 +14,8 @@ public class DrugBoxApplication {
         SpringApplication.run(DrugBoxApplication.class, args);
     }
 
+    @Bean
+    public InMemoryHttpTraceRepository httpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    }
 }
